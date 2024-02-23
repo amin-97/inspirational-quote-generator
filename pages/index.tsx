e9bd02ee06787301e3c33ba2a71ecafb8ff3966f
Amin Khan <64826872+amin-97@users.noreply.github.com>
2024-02-22 23:45:02 -0500

    added api + lambda +auth + graphql

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,29 @@
+import React, { useState } from "react";
 import Head from "next/head";
 import Image from "next/image";
 import styles from "@/styles/Home.module.css";
-import { GradientBackgroundCon } from "@/components/QuoteGenerator/QuoteGeneratorElements";
+//Components
+import {
+  BackgroundImage1,
+  BackgroundImage2,
+  GradientBackgroundCon,
+  FooterCon,
+  FooterLink,
+  QuoteGeneratorCon,
+  QuoteGeneratorInnerCon,
+  QuoteGeneratorTitle,
+  QuoteGeneratorSubTitle,
+  GenerateQuoteButton,
+  GenerateQuoteButtonText,
+  QuoteGeneratorModalInnerCon,
+} from "@/components/QuoteGenerator/QuoteGeneratorElements";
+//Assets
+import Clouds1 from "../assets/cloud-and-thunder.png";
+import Clouds2 from "../assets/cloudy-weather.png";
 
 export default function Home() {
+  const [numberOfQuotes, setNumberOfQuotes] = useState<Number | null>(0);
+
   return (
     <>
       <Head>
@@ -13,7 +33,57 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       {/* Background */}
-      <GradientBackgroundCon></GradientBackgroundCon>
+      <GradientBackgroundCon>
+        {/* Quote Generator Modal Pop-Up*/}
+        {/* <QuoteGeneratorModal /> */}
+
+        {/* Quote Generator */}
+        <QuoteGeneratorCon>
+          <QuoteGeneratorInnerCon>
+            <QuoteGeneratorTitle>
+              Daily Inspiration Generator
+            </QuoteGeneratorTitle>
+            <QuoteGeneratorSubTitle>
+              {" "}
+              Looking for a splash of inspiration? Generate a quote card with a
+              random inspirational quote provided by{" "}
+              <FooterLink
+                href="https://zenquotes.io/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                ZenQuotes API
+              </FooterLink>
+              .
+              <GenerateQuoteButton>
+                <GenerateQuoteButtonText onClick={null}>
+                  Make a Quote
+                </GenerateQuoteButtonText>
+              </GenerateQuoteButton>
+            </QuoteGeneratorSubTitle>
+          </QuoteGeneratorInnerCon>
+        </QuoteGeneratorCon>
+
+        {/* Background Images */}
+        <BackgroundImage1 src={Clouds1} height="300" alt="cloudybackground1" />
+        <BackgroundImage2 src={Clouds2} height="300" alt="cloudybackground2" />
+
+        {/* Footer */}
+        <FooterCon>
+          <>
+            Quotes Generated: {numberOfQuotes}
+            <br />
+            Developed with care by{" "}
+            <FooterLink
+              href="https://www.linkedin.com/in/aminkhan12/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              @AminKhan
+            </FooterLink>
+          </>
+        </FooterCon>
+      </GradientBackgroundCon>
     </>
   );
 }
